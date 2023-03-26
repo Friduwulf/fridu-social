e1ec57a14623fc8c8bef661ad1864c9c8eee007e
Devin <devinkearney@gmail.com>
2023-03-26 10:38:58 -0300

    first pass of thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+} = require('../../controllers/thoughtsController');
+
+router.route('/')
+   .get(getThoughts)
+   .post(createThought);
+
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
