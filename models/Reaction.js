3197f68f69b313a057407b49b8b4b70b2f38e294
Devin <devinkearney@gmail.com>
2023-03-26 09:30:11 -0300

    first pass of models

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const { Schema, Types } = require('mongoose');
+
+//Schema for what makes a reaction
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+    }
+);
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+//Schema for what makes up a thought
+const thoughtSchema = new schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+            trim: true,
+        },
+        createdAt: {
+            date: {
+                type: Date,
+                default: Date.now,
+            }
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        reactions: [reactionsSchema],
+        toJSON: {
+                getters: true
+            },
+            id: false
+    }
+);
+
+//Virtual for reaction count
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
